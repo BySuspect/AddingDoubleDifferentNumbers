1b9619ceb645dc34861140a6633287f66bc588fe
BySuspect <70052500+BySuspect@users.noreply.github.com>
2022-09-03 18:38:51 +0300

    Add project files.

---
+++ AddingDoubleDifferentNumbers/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace AddingDoubleDifferentNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvCollection = new System.Windows.Forms.ListView();
+            this.Number1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Number2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblnum1 = new System.Windows.Forms.Label();
+            this.lblnum2 = new System.Windows.Forms.Label();
+            this.txtnum1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnum2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvCollection
+            // 
+            this.lvCollection.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Number1,
+            this.Number2});
+            this.lvCollection.FullRowSelect = true;
+            this.lvCollection.GridLines = true;
+            this.lvCollection.HideSelection = false;
+            this.lvCollection.Location = new System.Drawing.Point(13, 13);
+            this.lvCollection.MultiSelect = false;
+            this.lvCollection.Name = "lvCollection";
+            this.lvCollection.Size = new System.Drawing.Size(121, 271);
+            this.lvCollection.TabIndex = 0;
+            this.lvCollection.TabStop = false;
+            this.lvCollection.UseCompatibleStateImageBehavior = false;
+            this.lvCollection.View = System.Windows.Forms.View.Details;
+            this.lvCollection.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // Number1
+            // 
+            this.Number1.Text = "Number 1";
+            // 
+            // Number2
+            // 
+            this.Number2.Text = "Number 2";
+            // 
+            // lblnum1
+            // 
+            this.lblnum1.AutoSize = true;
+            this.lblnum1.Location = new System.Drawing.Point(13, 291);
+            this.lblnum1.Name = "lblnum1";
+            this.lblnum1.Size = new System.Drawing.Size(35, 13);
+            this.lblnum1.TabIndex = 1;
+            this.lblnum1.Text = "label1";
+            // 
+            // lblnum2
+            // 
+            this.lblnum2.AutoSize = true;
+            this.lblnum2.Location = new System.Drawing.Point(65, 291);
+            this.lblnum2.Name = "lblnum2";
+            this.lblnum2.Size = new System.Drawing.Size(35, 13);
+            this.lblnum2.TabIndex = 1;
+            this.lblnum2.Text = "label1";
+            // 
+            // txtnum1
+            // 
+            this.txtnum1.Location = new System.Drawing.Point(201, 12);
+            this.txtnum1.Name = "txtnum1";
+            this.txtnum1.Size = new System.Drawing.Size(100, 20);
+            this.txtnum1.TabIndex = 0;
+            this.txtnum1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(160, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Num 1";
+            // 
+            // txtnum2
+            // 
+            this.txtnum2.Location = new System.Drawing.Point(201, 36);
+            this.txtnum2.Name = "txtnum2";
+            this.txtnum2.Size = new System.Drawing.Size(100, 20);
+            this.txtnum2.TabIndex = 1;
+            this.txtnum2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(160, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Num 2";
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(201, 62);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(100, 23);
+            this.btnadd.TabIndex = 2;
+            this.btnadd.Text = "ADD";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            this.btnadd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 313);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtnum2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnum1);
+            this.Controls.Add(this.lblnum2);
+            this.Controls.Add(this.lblnum1);
+            this.Controls.Add(this.lvCollection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvCollection;
+        private System.Windows.Forms.Label lblnum1;
+        private System.Windows.Forms.Label lblnum2;
+        private System.Windows.Forms.TextBox txtnum1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnum2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.ColumnHeader Number1;
+        private System.Windows.Forms.ColumnHeader Number2;
+    }
+}
+
---
+++ AddingDoubleDifferentNumbers/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddingDoubleDifferentNumbers
+{
+    public partial class Form1 : Form
+    {
+        int num1 = 0, num2 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtnum1.Text.Trim() != String.Empty && txtnum2.Text.Trim() != String.Empty)
+                {
+                    num1 += Convert.ToInt32(txtnum1.Text);
+                    num2 += Convert.ToInt32(txtnum2.Text);
+
+                    lblnum1.Text = num1.ToString();
+                    lblnum2.Text = num2.ToString();
+
+                    var lvItem = new ListViewItem();
+                    lvItem.Text = txtnum1.Text;
+                    lvItem.SubItems.Add(txtnum2.Text);
+                    lvCollection.Items.Add(lvItem);
+
+                    txtnum1.Text = String.Empty;
+                    txtnum2.Text = String.Empty;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnadd_Click(null, null);
+                txtnum1.Focus();
+            }
+        }
+    }
+}
